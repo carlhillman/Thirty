ecd9b01f9c5c14ba099a8f938fabbfdc3bc242d0
Carl Hillman <carl.hillman@gmail.com>
2020-07-06 21:45:11 +0200

    Pushing all files to git

--- app/src/main/java/se/umu/carl/thirty/ResultFragment.java
+++ app/src/main/java/se/umu/carl/thirty/ResultFragment.java
@@ -1,6 +1,7 @@
 package se.umu.carl.thirty;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,148 +18,198 @@ import java.util.List;
 import se.umu.carl.thirty.Models.ResultStorage;
 
 public class ResultFragment extends Fragment {
+
+    TextView choiceOne;
+    TextView choiceTwo;
+    TextView choiceThree;
+    TextView choiceFour;
+    TextView choiceFive;
+    TextView choiceSix;
+    TextView choiceSeven;
+    TextView choiceEight;
+    TextView choiceNine;
+    TextView choiceTen;
+    TextView summaryText;
+
+    TextView scoreOne;
+    TextView scoreTwo;
+    TextView scoreThree;
+    TextView scoreFour;
+    TextView scoreFive;
+    TextView scoreSix;
+    TextView scoreSeven;
+    TextView scoreEight;
+    TextView scoreNine;
+    TextView scoreTen;
+    TextView summaryScore;
+    Button btnRestart;
+int totalSum = 0;
     @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_result, container, false);
-        TextView choice1 = view.findViewById(R.id.choiceRow1);
-        TextView score1 = view.findViewById(R.id.scoreRow1);
 
-        TextView choice2 = view.findViewById(R.id.choiceRow2);
-        TextView score2 = view.findViewById(R.id.scoreRow2);
-
-        TextView choice3 = view.findViewById(R.id.choiceRow3);
-        TextView score3 = view.findViewById(R.id.scoreRow3);
-
-        TextView choice4 = view.findViewById(R.id.choiceRow4);
-        TextView score4 = view.findViewById(R.id.scoreRow4);
-
-        TextView choice5 = view.findViewById(R.id.choiceRow5);
-        TextView score5 = view.findViewById(R.id.scoreRow5);
-
-        TextView choice6 = view.findViewById(R.id.choiceRow6);
-        TextView score6 = view.findViewById(R.id.scoreRow6);
-
-        TextView choice7 = view.findViewById(R.id.choiceRow7);
-        TextView score7 = view.findViewById(R.id.scoreRow7);
-
-        TextView choice8 = view.findViewById(R.id.choiceRow8);
-        TextView score8 = view.findViewById(R.id.scoreRow8);
-
-        TextView choice9 = view.findViewById(R.id.choiceRow9);
-        TextView score9 = view.findViewById(R.id.scoreRow9);
-
-        TextView choice10 = view.findViewById(R.id.choiceRow10);
-        TextView score10 = view.findViewById(R.id.scoreRow10);
-
-        TextView summaryText = view.findViewById(R.id.txtTotalScore);
-        TextView summaryScore = view.findViewById(R.id.txtTotalScoreValue);
-
-        ArrayList<String> keys = new ArrayList<>(ResultStorage.choicePoints.keySet());
-        for (int index = 0; index < keys.size(); index++) {
-            switch (index) {
-                case 0:
-                    Object key1 = keys.get(index);
-                    choice1.setText(key1.toString());
-                    break;
-                case 1:
-                    Object key2 = keys.get(index);
-                    choice2.setText(key2.toString());
-                    break;
-                case 2:
-                    Object key3 = keys.get(index);
-                    choice3.setText(key3.toString());
-                    break;
-                case 3:
-                    Object key4 = keys.get(index);
-                    choice4.setText(key4.toString());
-                    break;
-                case 4:
-                    Object key5 = keys.get(index);
-                    choice5.setText(key5.toString());
-                    break;
-                case 5:
-                    Object key6 = keys.get(index);
-                    choice6.setText(key6.toString());
-                    break;
-                case 6:
-                    Object key7 = keys.get(index);
-                    choice7.setText(key7.toString());
-                    break;
-                case 7:
-                    Object key8 = keys.get(index);
-                    choice8.setText(key8.toString());
-                    break;
-                case 8:
-                    Object key9 = keys.get(index);
-                    choice9.setText(key9.toString());
-                    break;
-                case 9:
-                    Object key10 = keys.get(index);
-                    choice10.setText(key10.toString());
-                    break;
+        initUIElements(view);
+      /*  btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    getActivity().recreate();
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
             }
-        }
-        ArrayList<Integer> values = new ArrayList<>(ResultStorage.choicePoints.values());
-        int totalSum = sumList(values);
-        for (int index = 0; index < values.size(); index++) {
-            switch (index) {
-                case 0:
-                    Object value1 = values.get(index);
-                    score1.setText(value1.toString());
-                    break;
-                case 1:
-                    Object value2 = values.get(index);
-                    score2.setText(value2.toString());
-                    break;
-                case 2:
-                    Object value3 = values.get(index);
-                    score3.setText(value3.toString());
-                    break;
-                case 3:
-                    Object value4 = values.get(index);
-                    score4.setText(value4.toString());
-                    break;
-                case 4:
-                    Object value5 = values.get(index);
-                    score5.setText(value5.toString());
-                    break;
-                case 5:
-                    Object value6 = values.get(index);
-                    score6.setText(value6.toString());
-                    break;
-                case 6:
-                    Object value7 = values.get(index);
-                    score7.setText(value7.toString());
-                    break;
-                case 7:
-                    Object value8 = values.get(index);
-                    score8.setText(value8.toString());
-                    break;
-                case 8:
-                    Object value9 = values.get(index);
-                    score9.setText(value9.toString());
-                    break;
-                case 9:
-                    Object value10 = values.get(index);
-                    score10.setText(value10.toString());
-                    break;
-            }
-        }
+        });
+       */
+        setColumnChoiceText();
+        setColumnChoiceValueResult();
         summaryText.setText(getResources().getString(R.string.totalScore));
         summaryScore.setText(String.valueOf(totalSum));
         return view;
     }
 
+    private void setColumnChoiceText(){
+        ArrayList<String> keys = new ArrayList<>(ResultStorage.choicePoints.keySet());
+        for (int index = 0; index < keys.size(); index++) {
+            switch (index) {
+                case 0:
+                    Object key1 = keys.get(index);
+                    choiceOne.setText(key1.toString());
+                    break;
+                case 1:
+                    Object key2 = keys.get(index);
+                    choiceTwo.setText(key2.toString());
+                    break;
+                case 2:
+                    Object key3 = keys.get(index);
+                    choiceThree.setText(key3.toString());
+                    break;
+                case 3:
+                    Object key4 = keys.get(index);
+                    choiceFour.setText(key4.toString());
+                    break;
+                case 4:
+                    Object key5 = keys.get(index);
+                    choiceFive.setText(key5.toString());
+                    break;
+                case 5:
+                    Object key6 = keys.get(index);
+                    choiceSix.setText(key6.toString());
+                    break;
+                case 6:
+                    Object key7 = keys.get(index);
+                    choiceSeven.setText(key7.toString());
+                    break;
+                case 7:
+                    Object key8 = keys.get(index);
+                    choiceEight.setText(key8.toString());
+                    break;
+                case 8:
+                    Object key9 = keys.get(index);
+                    choiceNine.setText(key9.toString());
+                    break;
+                case 9:
+                    Object key10 = keys.get(index);
+                    choiceTen.setText(key10.toString());
+                    break;
+            }
+        }
+    }
+
+    private void setColumnChoiceValueResult(){
+        ArrayList<Integer> values = new ArrayList<>(ResultStorage.choicePoints.values());
+        totalSum = sumList(values);
+        for (int index = 0; index < values.size(); index++) {
+            switch (index) {
+                case 0:
+                    Object value1 = values.get(index);
+                    scoreOne.setText(value1.toString());
+                    break;
+                case 1:
+                    Object value2 = values.get(index);
+                    scoreTwo.setText(value2.toString());
+                    break;
+                case 2:
+                    Object value3 = values.get(index);
+                    scoreThree.setText(value3.toString());
+                    break;
+                case 3:
+                    Object value4 = values.get(index);
+                    scoreFour.setText(value4.toString());
+                    break;
+                case 4:
+                    Object value5 = values.get(index);
+                    scoreFive.setText(value5.toString());
+                    break;
+                case 5:
+                    Object value6 = values.get(index);
+                    scoreSix.setText(value6.toString());
+                    break;
+                case 6:
+                    Object value7 = values.get(index);
+                    scoreSeven.setText(value7.toString());
+                    break;
+                case 7:
+                    Object value8 = values.get(index);
+                    scoreEight.setText(value8.toString());
+                    break;
+                case 8:
+                    Object value9 = values.get(index);
+                    scoreNine.setText(value9.toString());
+                    break;
+                case 9:
+                    Object value10 = values.get(index);
+                    scoreTen.setText(value10.toString());
+                    break;
+            }
+        }
+    }
+
+    private void initUIElements(View view) {
+        choiceOne = view.findViewById(R.id.choiceRow1);
+        scoreOne = view.findViewById(R.id.scoreRow1);
+
+        choiceTwo = view.findViewById(R.id.choiceRow2);
+        scoreTwo = view.findViewById(R.id.scoreRow2);
+
+        choiceThree = view.findViewById(R.id.choiceRow3);
+        scoreThree = view.findViewById(R.id.scoreRow3);
+
+        choiceFour = view.findViewById(R.id.choiceRow4);
+        scoreFour = view.findViewById(R.id.scoreRow4);
+
+        choiceFive = view.findViewById(R.id.choiceRow5);
+        scoreFive = view.findViewById(R.id.scoreRow5);
+
+        choiceSix = view.findViewById(R.id.choiceRow6);
+        scoreSix = view.findViewById(R.id.scoreRow6);
+
+        choiceSeven = view.findViewById(R.id.choiceRow7);
+        scoreSeven = view.findViewById(R.id.scoreRow7);
+
+        choiceEight = view.findViewById(R.id.choiceRow8);
+        scoreEight = view.findViewById(R.id.scoreRow8);
+
+        choiceNine = view.findViewById(R.id.choiceRow9);
+        scoreNine = view.findViewById(R.id.scoreRow9);
+
+        choiceTen = view.findViewById(R.id.choiceRow10);
+        scoreTen = view.findViewById(R.id.scoreRow10);
+
+        summaryText = view.findViewById(R.id.txtTotalScore);
+        summaryScore = view.findViewById(R.id.txtTotalScoreValue);
+
+        btnRestart = view.findViewById(R.id.btnRestart);
+    }
 
     /**
      * summera en lista av heltal
      *
-     * @param list - används för att veta vilket poängval som gjordes
+     * @param list - används för att summera den totala poängen
      * @return sum
      */
-
     private static int sumList(List<Integer> list) {
         int sum = 0;
         for (int integer : list) {
